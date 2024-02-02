cabd0b44da05e9de8e0aed3843c451a4b1755126
PPaczek <pawel.paczek20@gmail.com>
2024-02-02 15:55:37 +0100

    Job

--- Assets/_Project/Scripts/RotatingCubeSystem.cs
+++ Assets/_Project/Scripts/RotatingCubeSystem.cs
@@ -16,7 +16,39 @@ public partial struct RotatingCubeSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        foreach ((RefRW<LocalTransform> localTransform, RefRO<RotateSpeed> rotateSpeed) in SystemAPI.Query<RefRW<LocalTransform>, RefRO<RotateSpeed>>())
+        // foreach ((RefRW<LocalTransform> localTransform, RefRO<RotateSpeed> rotateSpeed) in SystemAPI.Query<RefRW<LocalTransform>, RefRO<RotateSpeed>>())
+        // {
+        //     float power = 1f;
+        //     for (int i = 0; i < 100000; i++)
+        //     {
+        //         power *= 2f;
+        //         power /= 2f;
+        //     }
+        //
+        //     localTransform.ValueRW = localTransform.ValueRO.RotateY(rotateSpeed.ValueRO.speedValue * SystemAPI.Time.DeltaTime * power);
+        // }
+
+        RotatingCubeJob rotatingCubeJob = new RotatingCubeJob
+        {
+            deltaTime = SystemAPI.Time.DeltaTime
+        };
+        
+        rotatingCubeJob.ScheduleParallel();
+    }
+
+    public void OnDestroy(ref SystemState state)
+    {
+    }
+    
+    [BurstCompile]
+    public partial struct RotatingCubeJob : IJobEntity
+    {
+        public float deltaTime;
+
+        //ref - is read/write
+        //in - is read only
+
+        public void Execute(ref LocalTransform localTransform, in RotateSpeed rotateSpeed)
         {
             float power = 1f;
             for (int i = 0; i < 100000; i++)
@@ -25,11 +57,7 @@ public partial struct RotatingCubeSystem : ISystem
                 power /= 2f;
             }
 
-            localTransform.ValueRW = localTransform.ValueRO.RotateY(rotateSpeed.ValueRO.speedValue * SystemAPI.Time.DeltaTime * power);
+            localTransform = localTransform.RotateY(rotateSpeed.speedValue * deltaTime * power);
         }
-    }
-
-    public void OnDestroy(ref SystemState state)
-    {
     }
 }
